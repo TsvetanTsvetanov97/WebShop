dd2c252b5a0a9b20d48f2e4e8f1de13041a70a80
Tsvetan Tsvetanov <tsvetan.tsvetanov@ingrammicro.com>
2019-08-15 01:17:30 +0300

    Initial Commit

---
+++ Shop/Shop.Data.Models/ShopUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Data.Models
+{
+    public class ShopUser : IdentityUser
+    {
+        public ShopUser()
+        {
+
+        }
+        
+        public string FullName { get; set; }
+    }
+}
---
+++ Shop/Shop.Data/ShopDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.Data
+{
+    public class ShopDbContext : IdentityDbContext<ShopUser, IdentityRole<string>, string>
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public ShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
